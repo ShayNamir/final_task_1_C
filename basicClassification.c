8fdfb89315c4ebf59acfcc208e7f4b63075ce85d
eilon315233 <156177204+eilon315233@users.noreply.github.com>
2024-01-25 10:23:11 +0200

    Update basicClassification.c

--- basicClassification.c
+++ basicClassification.c
@@ -14,9 +14,11 @@ int factorial(int x)
 
 /* will return if a number is prime*/
 int isPrime(int a) {
-    if (a <= 1) {
+
+    if (a < 1) 
         return 0; // Not a prime number
-    }
+    if(a==1)
+        return 1;// 1 is a prime number 
 
     for (int i = 2; i * i <= a; i++) {
         if (a % i == 0) {
